48f446ea43678c24aa7a556eaff8715a94f58e78
Sandeep Kumar <sandeepkumaramni@gmail.com>
2019-10-22 01:27:40 -0500

    Update snapImage.py

--- snapImage.py
+++ snapImage.py
@@ -17,7 +17,7 @@ class snapImage:
         image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         cv2.imwrite("screenshot/img"+str(self.i)+".png",map_img)
         # crop location from these below coordinates
-        r0 = 286;r1 = 814;r2 = 221;r3 = 223
+        r0 = 288;r1 = 812;r2 = 218;r3 = 224
         map_img = image[r1:r1+r3, r0:r0+r2]
 
         #save map_imgs if needed 
